626201b7b4b336d88c80ee030b7fe04847c7961c
pelincicek <pelin@ketencek.com>
2024-04-23 16:18:29 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { C1Component } from './components/c1/c1.component';
+import { c1Route } from './components/c1/c1.route';
+import { c2Route } from './components/c2/c2.route';
+import { c3Route } from './components/c3/c3.route';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const indexRoute: Route = {
+  path: '',
+  component: C1Component,
+};
+
+export const routes: Routes = [
+  indexRoute,
+  c1Route,
+  c2Route,
+  c3Route,
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/c2/c2.route.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { C2Component } from './c2.component';
+
+export const c2Route: Route = {
+  path: 'c2',
+  component: C2Component,
+};
